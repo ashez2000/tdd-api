f92f2e7516b6cf5dc7b8eb2afd981ef074c3c5e1
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-07-13 15:11:24 +0530

    setup simple fastify server

---
+++ src/main.js
@@ -0,0 +1,15 @@
+const server = require('./app')({
+  logger: {
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+    },
+  },
+})
+
+server.listen({ port: 3000 }, (err, address) => {
+  if (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+})
